74a24f09e377a680dad25b15a41f79741dc5b80e
LING <179689727@qq.com>
2019-08-07 18:40:01 +0800

    2.1 Game Logic Code

---
+++ main2048.js
@@ -0,0 +1,65 @@
+var board = new Array();
+var score = 0;
+
+$(document).ready(function () {
+    newGame();
+});
+
+function newGame() {
+    //initialize grid
+    init();
+
+    //gernerate number
+
+}
+
+function init() {
+    for(var i=0 ; i<4 ; i++) {
+        for (var j=0; j<4; j++) {
+
+            var gridCell = $("#grid-cell-" + i + "-" + j);
+            gridCell.css('top', getPosTop(i, j));
+            gridCell.css('left', getPosLeft(i, j));
+
+        }
+    }
+
+    for(var i=0 ; i<4 ; i++) {
+        board[i]=new Array();
+        for (var j=0; j<4; j++) {
+
+            board[i][j]=0;
+        }
+    }
+
+    updateBoardView();
+}
+
+function updateBoardView() {
+
+    $(".number-cell").remove();
+    for(var i=0 ; i<4 ; i++) {
+        for (var j=0; j<4; j++) {
+            $("#grid-container").append('<div class="number-cell"  id="number-cell-'+ i +'-'+j+'"></div>');
+            var theNumberCell = $('#number-cell-'+i+'-'+j);
+
+            if(board[i][j]==0){
+                theNumberCell.css('width','0px');
+                theNumberCell.css('height','0px');
+                theNumberCell.css('top','0px');
+                theNumberCell.css('left','0px');
+            }
+            else {
+                theNumberCell.css('width','0px');
+                theNumberCell.css('height','0px');
+                theNumberCell.css('top',getPosTop(i,j));
+                theNumberCell.css('left',getPosLeft(i,j));
+                theNumberCell.css('background-color',getNumberBackgroundColor(board[i][j]));
+                theNumberCell.css('color',getNumberColor(board[i][j]));
+                theNumberCell.css(borad[i][j]);
+
+            }
+
+        }
+    }
+}
